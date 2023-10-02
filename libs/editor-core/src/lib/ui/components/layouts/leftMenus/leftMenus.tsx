3dd909a063e63663b57da02bb68ba9f1c9114544
Behrooz Arabzadeh <behrooz.arabzadeh@nexontis.com>
2023-10-02 23:30:09 +0200

    update a file name

---
+++ libs/editor-core/src/lib/ui/components/layouts/leftMenus/leftMenus.tsx
@@ -0,0 +1,18 @@
+import { ManagerWithUiPlugin } from '@weblancer-ui/types';
+import { FunctionComponent } from 'react';
+import layoutStyle from '../../../styles/editorLayout.module.scss';
+
+export interface ILeftMenusProps {
+  managers: ManagerWithUiPlugin[];
+}
+
+export const LeftMenus: FunctionComponent<ILeftMenusProps> = ({ managers }) => {
+  return (
+    <div className={layoutStyle.leftMenus}>
+      Left menus
+      {managers.map(({ uiPlugin }) => {
+        return <div key={uiPlugin.name}></div>;
+      })}
+    </div>
+  );
+};
