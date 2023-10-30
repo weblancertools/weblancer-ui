cf1cb72d1371e397017139ed78d3700956bba382
Behrooz Arabzadeh <behrooz.arabzadeh@nexontis.com>
2023-10-30 14:09:35 +0100

    Enhanced layout tree view

--- libs/layout-manager/src/lib/ui/layoutDrawer/layoutDrawer.tsx
+++ libs/layout-manager/src/lib/ui/layoutDrawer/layoutDrawer.tsx
@@ -3,17 +3,43 @@ import styles from './layoutDrawer.module.scss';
 import { LayoutManager } from '../../layout-manager';
 import { ILayoutManagerActions } from '../../types';
 import { TreeView } from '@mui/x-tree-view/TreeView';
-import { TreeItem } from '@mui/x-tree-view';
+import {
+  TreeItem,
+  TreeItemContentProps,
+  TreeItemProps,
+  useTreeItem,
+} from '@mui/x-tree-view';
 import { IComponentData } from '@weblancer-ui/prop-manager';
+import {
+  AdjustmentManager,
+  IAdjustmentManagerActions,
+} from '@weblancer-ui/adjustment-manager';
+import { forwardRef } from 'react';
+import classNames from 'classnames';
 
 export const LayoutDrawer = () => {
   const layoutManager =
     useWeblancerEditorManager<ILayoutManagerActions>(LayoutManager);
+  const adjustmentManager =
+    useWeblancerEditorManager<IAdjustmentManagerActions>(AdjustmentManager);
+
   const pageData = layoutManager.getLayout();
+
+  const handleSelect = (
+    event: React.SyntheticEvent<Element, Event>,
+    nodeId: string
+  ) => {
+    adjustmentManager.setSelectedItemId(nodeId);
+  };
 
   return (
     <div className={styles.root}>
-      <TreeView>
+      <TreeView
+        onNodeSelect={handleSelect}
+        multiSelect={false}
+        defaultCollapseIcon={'▾'}
+        defaultExpandIcon={'▸'}
+      >
         <RenderLeaf componentData={pageData} />
       </TreeView>
     </div>
@@ -38,11 +64,87 @@ const RenderLeaf = ({
   );
 
   return (
-    <TreeItem
+    <CustomTreeItem
       nodeId={componentData.id}
       label={componentData.name ?? componentData.id}
+      onClick={(e) => {
+        e.preventDefault();
+      }}
     >
       {children}
-    </TreeItem>
+    </CustomTreeItem>
   );
 };
+
+const CustomTreeItem = forwardRef(function CustomTreeItem(
+  props: TreeItemProps,
+  ref: React.Ref<HTMLLIElement>
+) {
+  return <TreeItem ContentComponent={CustomLeaf} {...props} ref={ref} />;
+});
+
+const CustomLeaf = forwardRef(function CustomContent(
+  props: TreeItemContentProps,
+  ref
+) {
+  const {
+    classes,
+    className,
+    label,
+    nodeId,
+    icon: iconProp,
+    expansionIcon,
+    displayIcon,
+  } = props;
+
+  const {
+    disabled,
+    expanded,
+    selected,
+    focused,
+    handleExpansion,
+    handleSelection,
+    preventSelection,
+  } = useTreeItem(nodeId);
+
+  const icon = iconProp || expansionIcon || displayIcon;
+
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    preventSelection(event);
+  };
+
+  const handleExpansionClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    handleExpansion(event);
+  };
+
+  const handleSelectionClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    handleSelection(event);
+  };
+
+  return (
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      className={classNames(className, classes.root, {
+        [classes.expanded]: expanded,
+        [classes.selected]: selected,
+        [classes.focused]: focused,
+        [classes.disabled]: disabled,
+      })}
+      onMouseDown={handleMouseDown}
+      ref={ref as React.Ref<HTMLDivElement>}
+    >
+      <div onClick={handleExpansionClick} className={classes.iconContainer}>
+        {icon}
+      </div>
+      <span onClick={handleSelectionClick} className={classes.label}>
+        {label}
+      </span>
+    </div>
+  );
+});
