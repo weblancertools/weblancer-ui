61102cb611efea9feb3db7154e149545ce049c4c
Behrooz Arabzadeh <behrooz.arabzadeh@nexontis.com>
2023-10-31 13:25:29 +0100

    Minor fix

--- libs/component-manager/src/lib/ui/components/componentItem/componentItem.tsx
+++ libs/component-manager/src/lib/ui/components/componentItem/componentItem.tsx
@@ -1,10 +1,18 @@
 import classNames from 'classnames';
 import styles from './componentItem.module.scss';
 import { IComponentHolder } from '../../../types';
-import { useWeblancerContext } from '@weblancer-ui/editor-core';
+import {
+  useWeblancerContext,
+  useWeblancerEditorManager,
+} from '@weblancer-ui/editor-core';
 import { EditorAction } from '@weblancer-ui/undo-manager';
 import { CreateItemAction } from '../../../actions/CreateItemAction';
 import { useComponentDrawerContext } from '../../context/componentDrawerContext';
+import {
+  AdjustmentManager,
+  IAdjustmentManagerActions,
+} from '@weblancer-ui/adjustment-manager';
+import { IPropManagerActions, PropManager } from '@weblancer-ui/prop-manager';
 
 interface IComponentItemProps {
   component: IComponentHolder;
@@ -14,10 +22,22 @@ export const ComponentItem = ({ component }: IComponentItemProps) => {
   const { callEditorAction } = useWeblancerContext();
   const { onClose } = useComponentDrawerContext();
 
+  const adjustmentManager =
+    useWeblancerEditorManager<IAdjustmentManagerActions>(AdjustmentManager);
+  const propManager =
+    useWeblancerEditorManager<IPropManagerActions>(PropManager);
+
   const handleClick = () => {
+    const selectedItemId = adjustmentManager.getSelectedItemId();
+    const componentData = propManager.getComponent(selectedItemId ?? '');
+
+    const parentId = componentData?.metadata?.isContainer
+      ? componentData.id
+      : propManager.getPageData().id;
+
     const createItemAction = EditorAction.getActionInstance(
       CreateItemAction
-    ).prepare(component.key, 'page1', {
+    ).prepare(component.key, parentId, {
       x: 500,
       y: 200,
     });
