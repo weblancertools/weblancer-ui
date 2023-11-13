c3ff117106f02726bab037c41508bcd48ce164f3
Behrooz Arabzadeh <behrooz.arabzadeh@nexontis.com>
2023-11-13 14:22:18 +0100

    Some changes

--- libs/store-manager/src/lib/helpers.ts
+++ libs/store-manager/src/lib/helpers.ts
@@ -37,3 +37,23 @@ export const configureStore = (options: ConfigureStoreOptions) => {
 
   return store as Store;
 };
+
+export const configureMockStore = <TRootState>(preloadedState: TRootState) => {
+  const store = reduxConfigureStore({
+    reducer: createReducer({}),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    preloadedState: preloadedState as any,
+  }) as Store & InjectableStore;
+
+  store.asyncReducers = {};
+  store.injectReducer = (key: string, reducer: Reducer) => {
+    if (store.asyncReducers[key])
+      throw new Error(`Injecting 2 sliceReducer with same name: ${key}`);
+
+    store.asyncReducers[key] = reducer;
+    store.replaceReducer(createReducer(store.asyncReducers));
+    return store;
+  };
+
+  return store as Store;
+};
--- libs/test/src/lib/providers/editorProvider.tsx
+++ libs/test/src/lib/providers/editorProvider.tsx
@@ -1,17 +1,27 @@
-import { Store } from '@reduxjs/toolkit';
 import { EditorCore } from '@weblancer-ui/editor-core-ui';
-import { configureStore } from '@weblancer-ui/store-manager';
+import {
+  configureMockStore,
+  configureStore,
+} from '@weblancer-ui/store-manager';
 import { getWeblancerDefaultPlugins } from '@weblancer-ui/tool-kit';
 import { IEditorUIPlugin } from '@weblancer-ui/types';
 
-export const store = configureStore({
-  reducer: {},
-});
-
-export function editorProvider(mockStore?: Store, plugins?: IEditorUIPlugin[]) {
+export function EditorProvider<TStoreRootState>({
+  preloadedState,
+  plugins,
+}: {
+  preloadedState?: TStoreRootState;
+  plugins?: IEditorUIPlugin[];
+}) {
   return (
     <EditorCore
-      store={mockStore ?? store}
+      store={
+        preloadedState
+          ? configureMockStore(preloadedState)
+          : configureStore({
+              reducer: {},
+            })
+      }
       plugins={plugins ?? [...getWeblancerDefaultPlugins()]}
     />
   );
