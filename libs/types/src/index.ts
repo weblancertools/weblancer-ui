d3a7ad8c74bffb03bce274808ee69eb3db0ca7c4
Behrooz Arabzadeh <behrooz.arabzade@gmail.com>
2023-11-19 19:25:23 +0100

    minor fix

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -2,7 +2,7 @@ export * from './lib/ITypeInfo';
 export * from './lib/IManager';
 export * from './lib/IEditorUIPlugin';
 export * from './lib/IReduxStore';
-export * from './lib/IReduxSelector';
+export * from './lib/iReduxSelector';
 export * from './lib/WeblancerWindowType';
 export * from './lib/PropTypes';
 export * from './lib/Constants';
